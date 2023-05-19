84e1277a7bbd027e1bde2d0588c6b160c3010b1b
Dorsey B <dorseybec@gmail.com>
2023-05-18 20:20:32 -0700

    easy squeezy

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImageUrl, setdogImageUrl] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setdogImageUrl(data);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+  return <img alt="A Random Dog" src={dogImageUrl.message}></img>;
+}
+
+export default App;
